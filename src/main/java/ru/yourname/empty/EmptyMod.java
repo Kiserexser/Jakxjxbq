3df3abdae5904e799b481247da12ea5fd104f853
Kiserexser <towartors@mail.ru>
2026-06-27 02:08:59 +0500

    Create EmptyMod.java

---
+++ src/main/java/ru/yourname/empty/EmptyMod.java
@@ -0,0 +1,10 @@
+package ru.yourname.empty;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EmptyMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("[EmptyMod] Пустышка успешно загружена!");
+    }
+}
